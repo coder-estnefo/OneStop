f4860a91d74179b91fd38c8fb6e5d6489a16aaa9
coder-estnefo <ofentsemasita@gmail.com>
2021-02-10 16:10:54 +0200

    10/02/2021 16:10 created form group,then added some validations for adding property details

--- src/app/pages/owner/property/add-property/add-property.page.ts
+++ src/app/pages/owner/property/add-property/add-property.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-property',
@@ -9,9 +10,42 @@ export class AddPropertyPage implements OnInit {
 	showDetails = true;
 	showImages = false;
 
-	constructor() {}
+	propertyForm: FormGroup;
 
-	ngOnInit() {}
+	constructor(private formBuilder: FormBuilder) {}
+
+	ngOnInit() {
+		this.propertyForm = this.formBuilder.group({
+			province: ['', [
+				Validators.required,
+				Validators.pattern('^[A-Z a-z 0-9]+$')
+			]],
+			address: ['', [
+				Validators.required,
+				Validators.pattern('^[A-Z a-z 0-9]+$')
+			]],
+			price: ['', [
+				Validators.required,
+				Validators.pattern('^[0-9]+$')
+			]],
+			bedrooms: ['', [
+				Validators.required,
+				Validators.pattern('^[0-9]+$')
+			]],
+			bathrooms: ['', [
+				Validators.required,
+				Validators.pattern('^[0-9]+$')
+			]],
+			garages: ['', [
+				Validators.required,
+				Validators.pattern('^[0-9]+$')
+			]],
+			description: ['', [
+				Validators.required,
+				Validators.pattern('^[A-Z a-z 0-9]+$')
+			]],
+		})
+	}
 
 	continue() {
 		this.images();
