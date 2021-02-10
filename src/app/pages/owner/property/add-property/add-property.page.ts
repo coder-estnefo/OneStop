60b94789db91e36aa0e3d6a1662f6f9cbeaa47ff
coder-estnefo <ofentsemasita@gmail.com>
2021-02-10 19:58:46 +0200

    10/02/2021 19:57 created form array for dynamic image upload

--- src/app/pages/owner/property/add-property/add-property.page.ts
+++ src/app/pages/owner/property/add-property/add-property.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-property',
@@ -7,57 +7,58 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./add-property.page.scss'],
 })
 export class AddPropertyPage implements OnInit {
-	showDetails = true;
-	showImages = false;
+  showDetails = true;
+  showImages = false;
 
-	propertyForm: FormGroup;
+  propertyForm: FormGroup;
 
-	constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder) {}
 
-	ngOnInit() {
-		this.propertyForm = this.formBuilder.group({
-			province: ['', [
-				Validators.required,
-				Validators.pattern('^[A-Z a-z 0-9]+$')
-			]],
-			address: ['', [
-				Validators.required,
-				Validators.pattern('^[A-Z a-z 0-9]+$')
-			]],
-			price: ['', [
-				Validators.required,
-				Validators.pattern('^[0-9]+$')
-			]],
-			bedrooms: ['', [
-				Validators.required,
-				Validators.pattern('^[0-9]+$')
-			]],
-			bathrooms: ['', [
-				Validators.required,
-				Validators.pattern('^[0-9]+$')
-			]],
-			garages: ['', [
-				Validators.required,
-				Validators.pattern('^[0-9]+$')
-			]],
-			description: ['', [
-				Validators.required,
-				Validators.pattern('^[A-Z a-z 0-9]+$')
-			]],
-		})
-	}
+  ngOnInit() {
+    this.propertyForm = this.formBuilder.group({
+      province: [
+        '',
+        [Validators.required, Validators.pattern('^[A-Z a-z 0-9]+$')],
+      ],
+      address: [
+        '',
+        [Validators.required, Validators.pattern('^[A-Z a-z 0-9]+$')],
+      ],
+      price: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      bedrooms: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      bathrooms: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      garages: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      description: [
+        '',
+        [Validators.required, Validators.pattern('^[A-Z a-z 0-9]+$')],
+      ],
+      propertyImages: this.formBuilder.array([this.formBuilder.control('')]),
+    });
+  }
 
-	continue() {
-		this.images();
-	}
+  continue() {
+    this.images();
+  }
 
-	details() {
-		this.showDetails = true;
-		this.showImages = false;
-	}
+  details() {
+    this.showDetails = true;
+    this.showImages = false;
+  }
 
-	images() {
-		this.showDetails = false;
-		this.showImages = true;
-	}
+  images() {
+    this.showDetails = false;
+    this.showImages = true;
+  }
+
+  get propertyImages() {
+    return this.propertyForm.get('propertyImages') as FormArray;
+  }
+
+  addPropertyImage() {
+    this.propertyImages.push(this.formBuilder.control(''));
+  }
+
+  upload() {
+    console.log(this.propertyForm);
+  }
 }
