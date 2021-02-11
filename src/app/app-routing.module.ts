051dd99afdb5ac0e56c6c70627b52302d688b577
coder-estnefo <ofentsemasita@gmail.com>
2021-02-11 18:07:01 +0200

    removed extra ];

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,47 +2,62 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-	{
-		path: 'home',
-		loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-	},
-	{
-		path: '',
-		redirectTo: 'home',
-		pathMatch: 'full'
-	},
-	{
-		path: 'dashboard',
-		loadChildren: () => import('./pages/owner/property/dashboard/dashboard.module').then( m => m.DashboardPageModule)
-	},
-	{
-		path: 'add-property',
-		loadChildren: () => import('./pages/owner/property/add-property/add-property.module').then( m => m.AddPropertyPageModule)
-	},
-	{
-		path: 'view-properties',
-		loadChildren: () => import('./pages/owner/property/view-properties/view-properties.module').then( m => m.ViewPropertiesPageModule)
-	},
-	{
-		path: 'edit-property',
-		loadChildren: () => import('./pages/owner/property/edit-property/edit-property.module').then( m => m.EditPropertyPageModule)
-	},
-	{
-		path: 'view-appointments',
-		loadChildren: () => import('./pages/owner/property/view-appointments/view-appointments.module').then( m => m.ViewAppointmentsPageModule)
-	},
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./pages/owner/property/dashboard/dashboard.module').then(
+        (m) => m.DashboardPageModule
+      ),
+  },
+  {
+    path: 'add-property',
+    loadChildren: () =>
+      import('./pages/owner/property/add-property/add-property.module').then(
+        (m) => m.AddPropertyPageModule
+      ),
+  },
+  {
+    path: 'view-properties',
+    loadChildren: () =>
+      import(
+        './pages/owner/property/view-properties/view-properties.module'
+      ).then((m) => m.ViewPropertiesPageModule),
+  },
+  {
+    path: 'edit-property',
+    loadChildren: () =>
+      import('./pages/owner/property/edit-property/edit-property.module').then(
+        (m) => m.EditPropertyPageModule
+      ),
+  },
+  {
+    path: 'view-appointments',
+    loadChildren: () =>
+      import(
+        './pages/owner/property/view-appointments/view-appointments.module'
+      ).then((m) => m.ViewAppointmentsPageModule),
+  },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
   },
-];
-
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
