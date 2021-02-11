871248cf17418afbcb38e6f5a03d20d355997220
coder-estnefo <ofentsemasita@gmail.com>
2021-02-11 14:02:57 +0200

    11/02/2021 14:02 added the functionality for the onchange event for image input

--- src/app/pages/owner/property/add-property/add-property.page.ts
+++ src/app/pages/owner/property/add-property/add-property.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-property',
@@ -7,19 +9,22 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./add-property.page.scss'],
 })
 export class AddPropertyPage implements OnInit {
-  showDetails = true;
-  showImages = false;
+  hideDetails = false;
+  hideImages = true;
 
   propertyForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  imagesList = [];
+  imagesUrls = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private storage: AngularFireStorage
+  ) {}
 
   ngOnInit() {
     this.propertyForm = this.formBuilder.group({
-      province: [
-        '',
-        [Validators.required, Validators.pattern('^[A-Z a-z 0-9]+$')],
-      ],
+      province: ['', [Validators.required, Validators.pattern('^[A-Z a-z]+$')]],
       address: [
         '',
         [Validators.required, Validators.pattern('^[A-Z a-z 0-9]+$')],
@@ -41,13 +46,13 @@ export class AddPropertyPage implements OnInit {
   }
 
   details() {
-    this.showDetails = true;
-    this.showImages = false;
+    this.hideDetails = false;
+    this.hideImages = true;
   }
 
   images() {
-    this.showDetails = false;
-    this.showImages = true;
+    this.hideDetails = true;
+    this.hideImages = false;
   }
 
   get propertyImages() {
@@ -60,5 +65,34 @@ export class AddPropertyPage implements OnInit {
 
   upload() {
     console.log(this.propertyForm);
+
+    console.log(this.imagesUrls);
+  }
+
+  reset() {
+    this.propertyImages.clear();
+  }
+
+  uploadFile(event) {
+    const file = event.target.files[0];
+    this.imagesList.push(file);
+
+    const fileName = file.name;
+    const fileExt = fileName.split('.').pop();
+    const filename = Math.random().toString(36).substring(2) + '.' + fileExt;
+    const filePath = 'properties/' + filename;
+    const task = this.storage.upload(filePath, file);
+    const ref = this.storage.ref(filePath);
+
+    task
+      .snapshotChanges()
+      .pipe(
+        finalize(() => {
+          ref.getDownloadURL().subscribe((downloadURL) => {
+            this.imagesUrls.push(downloadURL);
+          });
+        })
+      )
+      .subscribe();
   }
 }
