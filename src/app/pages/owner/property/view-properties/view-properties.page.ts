cd264d21840119d9e285781811c5dea299d884b1
coder-estnefo <ofentsemasita@gmail.com>
2021-02-12 00:24:14 +0200

    12/02/2021 00:24 - added the functionality for retrieving properties for a specific user then displaying them

--- src/app/pages/owner/property/view-properties/view-properties.page.ts
+++ src/app/pages/owner/property/view-properties/view-properties.page.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { PropertyService } from 'src/app/services/property/property.service';
+
+export interface property {
+  ownerID: number;
+  province: string;
+  address: string;
+  price: number;
+  bedrooms: number;
+  bathrooms: number;
+  garages: number;
+  description: string;
+  images: string[];
+  availability_status: string;
+  popular: boolean;
+}
 
 @Component({
   selector: 'app-view-properties',
@@ -6,10 +21,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-properties.page.scss'],
 })
 export class ViewPropertiesPage implements OnInit {
+  properties = [];
 
-  constructor() { }
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400,
+  };
+
+  constructor(private propertyService: PropertyService) {}
 
   ngOnInit() {
+    this.propertyService.getOwnerProperties(678290).subscribe((array) => {
+      this.properties = array.map((property) => {
+        return ({
+          id: property.payload.doc.id,
+          ...(property.payload.doc.data() as Object),
+        } as unknown) as property;
+      });
+    });
   }
-
 }
