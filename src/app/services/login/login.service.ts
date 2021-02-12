5e0d6a340cd48f293e2e9c01fa0c00912adebe89
coder-estnefo <ofentsemasita@gmail.com>
2021-02-12 23:51:49 +0200

    12/02/2021 23:51 - created a login and registration service

---
+++ src/app/services/login/login.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  state;
+
+  constructor(
+    private fireAuth: AngularFireAuth,
+    private firestore: AngularFirestore
+  ) {}
+
+  register(email, password, name, surname) {
+    return this.fireAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then((results) => {
+        this.getAuthState();
+        let userID = results.user.uid;
+        return this.firestore.collection('User').add({
+          userID,
+          name,
+          surname,
+        });
+      });
+  }
+
+  login(email, password) {
+    return this.fireAuth
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.getAuthState();
+      });
+  }
+
+  getAuthState() {
+    this.fireAuth.authState.subscribe((state) => {
+      this.state = state;
+    });
+  }
+
+  getUserID() {
+    return this.state.uid;
+  }
+}
