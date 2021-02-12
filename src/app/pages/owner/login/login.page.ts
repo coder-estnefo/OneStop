81c72c5f06fc1ec5a9d70f342e2fb2d94a36a097
coder-estnefo <ofentsemasita@gmail.com>
2021-02-12 16:42:33 +0200

    12/02/2021 16:42 - created login component with login form

---
+++ src/app/pages/owner/login/login.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'), //Not 100%
+        ],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+}
