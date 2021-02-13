f1c27a8f847cd58b9f7a9b9584860b12c10f29f1
coder-estnefo <ofentsemasita@gmail.com>
2021-02-13 12:49:25 +0200

    13/02/2021 12:48 - added the functionality for registration

--- src/app/pages/owner/register/register.page.ts
+++ src/app/pages/owner/register/register.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { LoginService } from 'src/app/services/login/login.service';
 
 @Component({
   selector: 'app-register',
@@ -8,8 +11,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterPage implements OnInit {
   registerForm: FormGroup;
-
-  constructor(private formBuilder: FormBuilder) {}
+  isLoading = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private loginService: LoginService,
+    public alertController: AlertController,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
@@ -24,5 +32,36 @@ export class RegisterPage implements OnInit {
       ],
       password: ['', [Validators.required, Validators.minLength(6)]],
     });
+  }
+
+  register() {
+    this.isLoading = true;
+    const { name, surname, email, password } = this.registerForm.value;
+
+    this.loginService
+      .register(email, password, name, surname)
+      .then(() => {
+        this.isLoading = false;
+        this.router.navigate(['/navigation'], {
+          queryParams: { status: 'success' },
+        });
+      })
+      .catch((error) => {
+        this.isLoading = false;
+        let code = error.code;
+        let errorMessage = error.message;
+        this.presentAlert(errorMessage);
+      });
+  }
+
+  async presentAlert(msg) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      message: `${msg} !`,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
   }
 }
