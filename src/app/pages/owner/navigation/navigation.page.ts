2fee7c76d44678cc740a875e08295379ceaa035e
coder-estnefo <ofentsemasita@gmail.com>
2021-02-13 13:19:23 +0200

    13/02/2021 13:19 - added functionality to display welcome message when logged in

--- src/app/pages/owner/navigation/navigation.page.ts
+++ src/app/pages/owner/navigation/navigation.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-navigation',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.page.scss'],
 })
 export class NavigationPage implements OnInit {
+  status;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    public alertController: AlertController,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.status = params.status;
+    });
+
+    if (this.status === 'success') {
+      this.presentAlert();
+    }
   }
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      message: 'Welcome to OneStop :)',
+      buttons: [
+        {
+          text: 'Continue',
+          handler: () => {
+            this.router.navigate(['/navigation']);
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
 }
