d22f859b5ec8467a373f70d0a87b6db7092d6cf1
coder-estnefo <ofentsemasita@gmail.com>
2021-03-14 23:35:29 +0200

    14/03/2021 23:35 - add: property chats

---
+++ src/app/pages/owner/property/chats/chats.page.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login/login.service';
+import { PropertyService } from 'src/app/services/property/property.service';
+
+@Component({
+  selector: 'app-chats',
+  templateUrl: './chats.page.html',
+  styleUrls: ['./chats.page.scss'],
+})
+export class ChatsPage implements OnInit {
+  userID;
+  propertyChats = [];
+
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private propertyService: PropertyService,
+    private auth: AngularFireAuth
+  ) {}
+
+  ngOnInit() {
+    this.auth.authState.subscribe((user) => {
+      if (user) {
+        this.userID = user.uid;
+        this.getPropertiesChats();
+      }
+    });
+  }
+
+  logout() {
+    this.loginService.logout().then(() => {
+      this.router.navigate(['/login']);
+    });
+  }
+
+  getPropertiesChats() {
+    this.propertyService.getChats(this.userID).subscribe((response) => {
+      this.propertyChats = response.map((chats) => {
+        return {
+          id: chats.payload.doc.id,
+          ...(chats.payload.doc.data() as Object),
+        };
+      });
+
+      const temp_chats = this.propertyChats.sort((a, b) => b.date - a.date);
+      this.propertyChats = temp_chats;
+    });
+  }
+
+  toMessages(chat) {
+    const { id, to, from } = chat;
+    this.router.navigate(['messages'], {
+      queryParams: { propertyID: id, userID: to, sendTo: from },
+    });
+  }
+}
